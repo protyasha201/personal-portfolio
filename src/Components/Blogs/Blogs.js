f93e01551fe8b2a279d392f9415f47dc462dc751
protyasha201 <protyasha0999@gmail.com>
2021-06-08 21:30:08 +0600

    updated portfolio

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,31 +1,59 @@
-import React from 'react';
-import './Blogs.css';
+import React from "react";
+import "./Blogs.css";
 
 const Blogs = () => {
-    const blogs = [
-        {
-            title: 'Making Creative and beautiful websites',
-            des: `Nowadays, websites are a major part of our internet. It's worldwide. In our day-to-day life, we use many websites.`,
-            link: 'https://protyashablogs.blogspot.com/2021/05/making-beautiful-and-creative-websites.html'
-        },
-    ]
-    return (
-        <section className="blogsSection">
-            <div>
-                <h1 className="text-white roboto-condensed blogsTitle">Blogs</h1>
+  const blogs = [
+    {
+      title: "Making Creative and beautiful websites",
+      des: `Nowadays, websites are a major part of our internet. It's worldwide. In our day-to-day life, we use many websites...`,
+      link: "https://protyashablogs.blogspot.com/2021/05/making-beautiful-and-creative-websites.html",
+    },
+    {
+      title: "Some Confusing and Important topics in Javascript",
+      des: `Many times we face some really confusing things while programming. And in Javascript, we often face such types of confusing topics. These…`,
+      link: "https://protyasha0999.medium.com/some-confusing-and-important-topics-in-javascript-187828b775b1",
+    },
+    {
+      title: "Optimizing Performance",
+      des: `Internally, React uses several clever techniques to minimize the number of costly DOM operations required to update the UI. For many…`,
+      link: "https://protyasha0999.medium.com/optimizing-performance-5287ad99ca28",
+    },
+    {
+      title: "Let’s Catch Some Errors.",
+      des: `We are programmers, cool huh? Yeah sounds pretty cool. But when it’s about getting some errors in our program, we feel like “oh come on…`,
+      link: "https://protyasha0999.medium.com/lets-catch-some-errors-afcc8b2e65ca",
+    },
+    {
+      title: "Array and methods for array in Javascript.",
+      des: `Arrays are well-knowned in programming world. It’s a data type. But it’s really a complicated concept to understand at the first place. But…`,
+      link: "https://protyasha0999.medium.com/array-and-methods-for-array-in-javascript-43d265225048",
+    },
+  ];
+  return (
+    <section className="blogsSection">
+      <div>
+        <h1 className="text-white roboto-condensed blogsTitle">Blogs</h1>
+      </div>
+      <div className="blogsContainer">
+        {blogs.map((blog) => (
+          <div key={blog.title} className="width-80 blogs">
+            <div className="h-75">
+              <h1 className="text-coral roboto-condensed">{blog.title}</h1>
+              <p className="text-gray montserrat">{blog.des}</p>
             </div>
-            <div className="blogsContainer">
-                {
-                    blogs.map(blog => 
-                    <div key={blog.title} className="width-80 blogs">
-                        <h1 className="text-coral roboto-condensed">{blog.title}</h1>
-                        <p className="text-gray montserrat">{blog.des}</p>
-                        <a rel="noreferrer" target="_blank" className="borderWhite p-5 brr-5" href={`${blog.link}`}>View Blog</a>
-                    </div>)
-                }
-            </div>
-        </section>
-    );
+            <a
+              rel="noreferrer"
+              target="_blank"
+              className="borderWhite p-5 brr-5"
+              href={`${blog.link}`}
+            >
+              View Blog
+            </a>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Blogs;
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,150 +1,184 @@
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import './Projects.css';
-import travel360 from '../../images/travel-360.png';
-import stairway from '../../images/stairway-heaven.png';
-import godoth from '../../images/godoth-jorney.png';
-import codePunk from '../../images/code-punk.png';
-import calculator from '../../images/calculator.png';
-import hungryMonster from '../../images/hungry-monster.png';
-import pinGenerator from '../../images/pin-generator.png';
-import clock from '../../images/clock.png';
-import drums from '../../images/virtual-drums.png';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./Projects.css";
+import travel360 from "../../images/travel-360.png";
+import stairway from "../../images/stairway-heaven.png";
+import godoth from "../../images/godoth-jorney.png";
+import codePunk from "../../images/code-punk.png";
+import calculator from "../../images/calculator.png";
+import hungryMonster from "../../images/hungry-monster.png";
+import pinGenerator from "../../images/pin-generator.png";
+import clock from "../../images/clock.png";
+import drums from "../../images/virtual-drums.png";
+import regmeet from "../../images/regmeet.png";
+import friendBook from "../../images/friendbook.jpg";
 
 const Projects = () => {
-    const fullStack = [
-        {
-            title: 'Travel 360',
-            des: 'A travel management website.',
-            img: travel360,
-            liveLink: 'https://travel-360-81e93.web.app/'
-        },
-        {
-            title: 'Stairway Heaven',
-            des: 'E-commerce site with admin panel',
-            img: stairway,
-            liveLink: 'https://stairway-heaven.web.app/'
-        },
-        {
-            title: 'Godoth Journey',
-            des: 'Vehicle service website with google map',
-            img: godoth,
-            liveLink: 'https://godoth-journey-5c449.web.app/'
-        },
-    ]
+  const fullStack = [
+    {
+      title: "Travel 360",
+      des: "A travel management website.",
+      img: travel360,
+      liveLink: "https://travel-360-81e93.web.app/",
+    },
+    {
+      title: "Stairway Heaven",
+      des: "E-commerce site with admin panel",
+      img: stairway,
+      liveLink: "https://stairway-heaven.web.app/",
+    },
+    {
+      title: "Godoth Journey",
+      des: "Vehicle service website with google map",
+      img: godoth,
+      liveLink: "https://godoth-journey-5c449.web.app/",
+    },
+  ];
 
-    const webApps = [
-        {
-            title: 'Code Punk',
-            des: 'A Code Editor to make websites.',
-            img: codePunk,
-            liveLink: 'https://protyasha201.github.io/code-punk/'
-        },
-        {
-            title: 'Virtual Drums',
-            des: 'Virtual Drum kit with mouseover.',
-            img: drums,
-            liveLink: 'https://protyasha201.github.io/virtual-drums/'
-        },
-        {
-            title: 'Calculator',
-            des: 'A Simple Calculator web app.',
-            img: calculator,
-            liveLink: 'https://protyasha201.github.io/Calculator/'
-        },
-        {
-            title: 'Clock',
-            des: 'This is a virtual clock with some animation',
-            img: clock,
-            liveLink: 'https://protyasha201.github.io/virtual-clock/'
-        },
-        {
-            title: 'Pin Generator',
-            des: 'A Pin Generator Web app.',
-            img: pinGenerator,
-            liveLink: 'https://protyasha201.github.io/pin-generator/'
-        },
-    ]
+  const androidApps = [
+    {
+      title: "FriendBook",
+      des: "A tinder application. Choose your dating partner or swipe",
+      img: friendBook,
+      liveLink: "https://github.com/YeasinHowladerEmon/Friendbook",
+    },
+  ];
 
-    const apiBased = [
-        {
-            title: 'Hungry Monster',
-            des: 'A online food shop website',
-            img: hungryMonster,
-            liveLink: 'https://protyasha201.github.io/Hungry-Monster/'
-        },
-    ]
+  const webApps = [
+    {
+      title: "RegMeet",
+      des: "This is a social media web app.",
+      img: regmeet,
+      liveLink: "https://regmeet.netlify.app/",
+    },
+    {
+      title: "Code Punk",
+      des: "A Code Editor to make websites.",
+      img: codePunk,
+      liveLink: "https://protyasha201.github.io/code-punk/",
+    },
+    {
+      title: "Virtual Drums",
+      des: "Virtual Drum kit with mouseover.",
+      img: drums,
+      liveLink: "https://protyasha201.github.io/virtual-drums/",
+    },
+    {
+      title: "Calculator",
+      des: "A Simple Calculator web app.",
+      img: calculator,
+      liveLink: "https://protyasha201.github.io/Calculator/",
+    },
+    {
+      title: "Clock",
+      des: "This is a virtual clock with some animation",
+      img: clock,
+      liveLink: "https://protyasha201.github.io/virtual-clock/",
+    },
+    {
+      title: "Pin Generator",
+      des: "A Pin Generator Web app.",
+      img: pinGenerator,
+      liveLink: "https://protyasha201.github.io/pin-generator/",
+    },
+  ];
 
-    return (
-        <section className="projectsSection">
-            <div className="projectsHeader d-flex justify-content-space align-center">
-                <h1 className="roboto-condensed headTitle text-coral">Projects</h1>
-                <a className="text-white f-30 hoverIcon transition" href="https://github.com/protyasha201">
-                    <span className="f-20 text-coral m-r-10">Codes</span>
-                    <FontAwesomeIcon icon={faGithub} />
-                </a>
+  const apiBased = [
+    {
+      title: "Hungry Monster",
+      des: "A online food shop website",
+      img: hungryMonster,
+      liveLink: "https://protyasha201.github.io/Hungry-Monster/",
+    },
+  ];
+
+  return (
+    <section className="projectsSection">
+      <div className="projectsHeader d-flex justify-content-space align-center">
+        <h1 className="roboto-condensed headTitle text-coral">Projects</h1>
+        <a
+          className="text-white f-30 hoverIcon transition"
+          href="https://github.com/protyasha201"
+        >
+          <span className="f-20 text-coral m-r-10">Codes</span>
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </div>
+
+      <div>
+        <h1 className="text-white montserrat">Full-Stack</h1>
+        <div className="projectsContainer">
+          {fullStack.map((project) => (
+            <div key={project.title} className="projects">
+              <div className="projectImg">
+                <img alt={project.title} src={project.img} />
+              </div>
+              <div className="p-10 projectDetail">
+                <h1 className="roboto-condensed">{project.title}</h1>
+                <p className="montserrat">{project.des}</p>
+                <a href={`${project.liveLink}`}>Preview</a>
+              </div>
             </div>
+          ))}
+        </div>
+      </div>
 
-            <div>
-                <h1 className="text-white montserrat">Full-Stack</h1>
-                <div className="projectsContainer">
-                    {
-                        fullStack.map(project =>
-                            <div key={project.title} className="projects">
-                                <div className="projectImg">
-                                    <img alt={project.title} src={project.img} />
-                                </div>
-                                <div className="p-10 projectDetail">
-                                    <h1 className="roboto-condensed">{project.title}</h1>
-                                    <p className="montserrat">{project.des}</p>
-                                    <a href={`${project.liveLink}`}>Preview</a>
-                                </div>
-                            </div>)
-                    }
-                </div>
+      <div>
+        <h1 className="text-white montserrat">Android Application</h1>
+        <div className="projectsContainer">
+          {androidApps.map((project) => (
+            <div key={project.title} className="projects">
+              <div className="projectImg">
+                <img alt={project.title} src={project.img} />
+              </div>
+              <div className="p-10 projectDetail">
+                <h1 className="roboto-condensed">{project.title}</h1>
+                <p className="montserrat">{project.des}</p>
+                <a href={`${project.liveLink}`}>Preview</a>
+              </div>
             </div>
+          ))}
+        </div>
+      </div>
 
-            <div>
-                <h1 className="text-white montserrat">Web Apps</h1>
-                <div className="projectsContainer">
-                    {
-                        webApps.map(project =>
-                            <div key={project.title} className="projects">
-                                <div className="projectImg">
-                                    <img alt={project.title} src={project.img} />
-                                </div>
-                                <div className="p-10 projectDetail">
-                                    <h1 className="roboto-condensed">{project.title}</h1>
-                                    <p className="montserrat">{project.des}</p>
-                                    <a href={`${project.liveLink}`}>Preview</a>
-                                </div>
-                            </div>)
-                    }
-                </div>
+      <div>
+        <h1 className="text-white montserrat">Web Apps</h1>
+        <div className="projectsContainer">
+          {webApps.map((project) => (
+            <div key={project.title} className="projects">
+              <div className="projectImg">
+                <img alt={project.title} src={project.img} />
+              </div>
+              <div className="p-10 projectDetail">
+                <h1 className="roboto-condensed">{project.title}</h1>
+                <p className="montserrat">{project.des}</p>
+                <a href={`${project.liveLink}`}>Preview</a>
+              </div>
             </div>
+          ))}
+        </div>
+      </div>
 
-            <div>
-                <h1 className="text-white montserrat">Api Based</h1>
-                <div className="projectsContainer">
-                    {
-                        apiBased.map(project =>
-                            <div key={project.title} className="projects">
-                                <div className="projectImg">
-                                    <img alt={project.title} src={project.img} />
-                                </div>
-                                <div className="p-10 projectDetail">
-                                    <h1 className="roboto-condensed">{project.title}</h1>
-                                    <p className="montserrat">{project.des}</p>
-                                    <a href={`${project.liveLink}`}>Preview</a>
-                                </div>
-                            </div>)
-                    }
-                </div>
+      <div>
+        <h1 className="text-white montserrat">Api Based</h1>
+        <div className="projectsContainer">
+          {apiBased.map((project) => (
+            <div key={project.title} className="projects">
+              <div className="projectImg">
+                <img alt={project.title} src={project.img} />
+              </div>
+              <div className="p-10 projectDetail">
+                <h1 className="roboto-condensed">{project.title}</h1>
+                <p className="montserrat">{project.des}</p>
+                <a href={`${project.liveLink}`}>Preview</a>
+              </div>
             </div>
-
-        </section>
-    );
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
